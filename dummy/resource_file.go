877977b0d7a8f39411a03485dc9b5b1e289311db
Chris Greiveldinger <christopher.greiveldinger@citrix.com>
2020-05-16 08:57:15 -0700

    Can input file name and contents

--- dummy/resource_file.go
+++ dummy/resource_file.go
@@ -1,9 +1,14 @@
 package dummy
 
 import (
+	"bufio"
+	"errors"
+	"io"
 	"io/ioutil"
 	"log"
+	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
@@ -12,9 +17,31 @@ func resourceFile() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceFileCreate,
 		Read:   resourceFileRead,
+		Update: resourceFileUpdate,
 		Delete: resourceFileDelete,
-		Schema: map[string]*schema.Schema{},
+		Schema: map[string]*schema.Schema{
+			"file_name": &schema.Schema {
+				Type: schema.TypeString,
+				Description: "Name of the file to create",
+				Required: true,
+				ForceNew: true,
+			},
+			"contents": &schema.Schema {
+				Type: schema.TypeString,
+				Description: "File contents",
+				Required: true,
+			},
+		},
 	}
+}
+
+const filePerm os.FileMode = 0644
+
+func formatContentsLine(c string) (string, error) {
+	if len(c) == 0 {
+		return "", errors.New("Cannot have empty string for contents")
+	}
+	return "CONTENTS: " + c, nil
 }
 
 func resourceFileCreate(d *schema.ResourceData, m interface{}) error {
@@ -23,10 +50,18 @@ func resourceFileCreate(d *schema.ResourceData, m interface{}) error {
 
 	config := m.(Config)
 
-	fullPath := filepath.Join(config.Directory, "step3.txt")
-	data := []byte("Test string\n")
+	// Not checking for existence of these keys, because Required is set in the schema.
+	fileName := d.Get("file_name").(string)
+	contents := d.Get("contents").(string)
 
-	err := ioutil.WriteFile(fullPath, data, 0644)
+	fullPath := filepath.Join(config.Directory, fileName)
+	cLine, err := formatContentsLine(contents)
+	if err != nil {
+		return err
+	}
+	data := []byte(cLine)
+
+	err = ioutil.WriteFile(fullPath, data, filePerm)
 	if err != nil {
 		return err
 	}
@@ -39,6 +74,7 @@ func resourceFileCreate(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceFileRead(d *schema.ResourceData, m interface{}) error {
+	log.Println("[DEBUG] resourceFileRead")
 	data, err := ioutil.ReadFile(d.Id())
 	if err != nil {
 		return err
@@ -47,6 +83,45 @@ func resourceFileRead(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
 
+func resourceFileUpdate(d *schema.ResourceData, m interface{}) error {
+	log.Println("[DEBUG] resourceFileUpdate")
+	if d.HasChange("contents") {
+		log.Println("[DEBUG] contents changed")
+		fileName := d.Id()
+		f, err := os.OpenFile(fileName, os.O_RDWR, filePerm)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		scanner := bufio.NewScanner(f)
+		newContents := ""
+		for scanner.Scan() {
+			line := scanner.Text()
+			if strings.Contains(line, "CONTENTS:") {
+				// Replace the CONTENTS string
+				line, err = formatContentsLine(d.Get("contents").(string))
+				if err != nil {
+					return err
+				}
+			}
+			newContents = newContents + line
+		}
+
+		f.Seek(0, io.SeekStart)
+
+		log.Printf("[DEBUG] writing\n\t%s", newContents)
+		n, err := f.WriteString(newContents)
+		if err != nil {
+			return err
+		}
+		log.Printf("[DEBUG] wrote %d bytes\n", n)
+		f.Sync()
+	}
+	return resourceFileRead(d, m)
+}
+
 func resourceFileDelete(d *schema.ResourceData, m interface{}) error {
-	return nil
+	log.Println("[DEBUG] resourceFileDelete")
+	return os.Remove(d.Id())
 }
